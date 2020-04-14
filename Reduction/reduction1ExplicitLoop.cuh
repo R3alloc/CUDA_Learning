e8db1669b3f5b4c3f6a01dc59c714b9d199790f8
yufande <yufande@gmail.com>
2020-04-14 23:40:34 +0800

    笔记：关于对数步长规约算法 该算法要求blockSize必须是2的整数次方，并且要做二次规约 纠正了tab占据空格的问题

--- Reduction/reduction1ExplicitLoop.cuh
+++ Reduction/reduction1ExplicitLoop.cuh
@@ -18,48 +18,48 @@
 __global__ void
 Reduction1_kernel( int *out, const int *in, size_t N )
 {
-	//这个数组的大小与blockSize有关（也就是blockDim.x）
-	//注意这个数组在这里定义的时候虽然没有指定大小，但是在调用这个kernel的时候有一个核函数参数就是用来控制kernel内部使用共享内存的大小。
-    extern __shared__ int sPartials[];	
+    //这个数组的大小与blockSize有关（也就是blockDim.x）
+    //注意这个数组在这里定义的时候虽然没有指定大小，但是在调用这个kernel的时候有一个核函数参数就是用来控制kernel内部使用共享内存的大小。
+    extern __shared__ int sPartials[];  
     int sum = 0;
-	//tid是当前线程在当前block中的索引
+    //tid是当前线程在当前block中的索引
     const int tid = threadIdx.x;
-	//i是当前线程在所有线程中的索引
-	//i的步长是grid当中的block数量*block中线程的数量
-	//in[]存储在全局内存中 输入指针被恰当地对齐，由这段代码发起的全部内存事务将被合并，这将最大限度地提高内存带宽。
-	//也就是说一个cuda线程要去多次访问全局内存，然后把这些值加起来
+    //i是当前线程在所有线程中的索引
+    //i的步长是grid当中的block数量*block中线程的数量
+    //in[]存储在全局内存中 输入指针被恰当地对齐，由这段代码发起的全部内存事务将被合并，这将最大限度地提高内存带宽。
+    //也就是说一个cuda线程要去多次访问全局内存，然后把这些值加起来
     for ( size_t i = blockIdx.x*blockDim.x + tid;
           i < N;
           i += blockDim.x*gridDim.x ) 
-	{
+    {
         sum += in[i];
     }
-	
-	//每个线程把它得到的累计值写入共享内存
+    
+    //每个线程把它得到的累计值写入共享内存
     sPartials[tid] = sum;
-	//在执行对数步长的规约前进行同步操作
+    //在执行对数步长的规约前进行同步操作
     __syncthreads();
 
-	//blockSize必须是2的整数次方的原因在这里：每一轮都只有上一次一半的线程还在工作
-	//对于共享内存中的值 执行对数步长的规约操作
-	//共享内存中后半部分的值被添加到前半部分的值上，
-	//假设blockDim.x == 1024，则第一轮activeThreads=512
+    //blockSize必须是2的整数次方的原因在这里：每一轮都只有上一次一半的线程还在工作
+    //对于共享内存中的值 执行对数步长的规约操作
+    //共享内存中后半部分的值被添加到前半部分的值上，
+    //假设blockDim.x == 1024，则第一轮activeThreads=512
     for ( int activeThreads = blockDim.x>>1; 
               activeThreads; 
               activeThreads >>= 1 ) //>>是二进制右移运算符 等价于整除2
-									//>>=是右移且赋值运算符 也就是activeThreads = activeThreads>>1
-	{
+                                    //>>=是右移且赋值运算符 也就是activeThreads = activeThreads>>1
+    {
         if ( tid < activeThreads ) 
-		{
+        {
             sPartials[tid] += sPartials[tid+activeThreads];
         }
-		//每一轮加完之后要线程同步
+        //每一轮加完之后要线程同步
         __syncthreads();
     }
-	
-	//每个block的0号线程存储一个结果，一共有numBlocks个线程，所以存储了这么多个结果。
+    
+    //每个block的0号线程存储一个结果，一共有numBlocks个线程，所以存储了这么多个结果。
     if ( tid == 0 ) 
-	{
+    {
         out[blockIdx.x] = sPartials[0];
     }
 }
@@ -71,11 +71,11 @@ Reduction1( int *answer, int *partial,
             int numBlocks, int numThreads )
 {
     unsigned int sharedSize = numThreads*sizeof(int);
-	//第一次的结果partial只是一个中间结果，并未完全做和
+    //第一次的结果partial只是一个中间结果，并未完全做和
     Reduction1_kernel<<< 
         numBlocks, numThreads, sharedSize>>>( 
             partial, in, N );
-	//第二次结果answer才是最终的计算结果。
+    //第二次结果answer才是最终的计算结果。
     Reduction1_kernel<<< 
         1, numThreads, sharedSize>>>( 
             answer, partial, numBlocks );
